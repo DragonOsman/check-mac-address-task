c492a70cd25c079ecb2366c1c80e6454fce15866
Osman Zakir <osmanzakir90@hotmail.com>
2020-01-30 01:57:18 +0500

    fixed regular expression

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const regexp = /([0-9A-F]{2}:[0-9A-F]{2}){3}/i;
+const regexp = /^(([0-9A-F]{2}[:]){5}[0-9A-F]{2})$/i;
 alert(regexp.test("01:32:54:67:89:AB"));
 alert(regexp.test("0132546789AB"));
 alert(regexp.test("01:32:54:67:89"));
