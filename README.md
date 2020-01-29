757f24c02342ddbafa9b5e791230f4696319d053
Osman Zakir <osmanzakir90@hotmail.com>
2020-01-29 19:44:51 +0500

    initial commit with all files

---
+++ README.md
@@ -0,0 +1,3 @@
+## Javascript.info Task "Check MAC-address" Solution
+
+This is my solution to the javascript.info Chapter RegExp Lesson "Capturing Groups" task "Check MAC-address".  
---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const regexp = /([0-9A-F]{2}:[0-9A-F]{2}){3}/i;
+alert(regexp.test("01:32:54:67:89:AB"));
+alert(regexp.test("0132546789AB"));
+alert(regexp.test("01:32:54:67:89"));
+alert(regexp.test("01:32:54:67:89:ZZ"));
